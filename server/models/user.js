097b4e6808b0a129b4db43151763d65f8cb8c585
websharp <websharp@websharps-iMac.local>
2018-10-14 10:38:41 -0400

    first commit

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-// To fix https://github.com/Automattic/mongoose/issues/4291
 mongoose.Promise = global.Promise;
 
 const userSchema = new Schema({
