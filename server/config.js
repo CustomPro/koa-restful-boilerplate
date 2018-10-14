2fc856acb0df50d096a3dce4154482a35bf03f36
custompro <kiros.mata@mail.com>
2018-10-14 12:29:35 -0400

    Add production mongo uri

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 export const port = process.env.PORT || 3000;
-export const connectionString = 'mongodb://localhost/koa-boilerplate';
+export const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/koa-boilerplate';
 export const baseApi = 'api';
